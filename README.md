7e051083cd06f8b5ed44859143e3c3e3014f59b2
Joseph Miller <joem@breachintelligence.com>
2020-10-22 07:41:33 -0500

    Committing initial integration

---
+++ README.md
@@ -0,0 +1,28 @@
+# Polarity hashdd NSRL RDS Lookup Integration
+
+![mode:on demand only](https://img.shields.io/badge/mode-on%20demand%20only-blue.svg)
+
+The Polarity hashdd integration allows for hash searches against the National Software Reference Library (NSRL) Reference Data Set (RDS) published by NIST.
+
+Register for a free account at the URL below:
+
+https://hashdd.com/#about
+
+Check out a snapshot if the integration below: 
+![image](https://user-images.githubusercontent.com/22529325/96872623-db52d280-1441-11eb-878d-95f24b0a60e6.png)
+
+## HashDD Integration Options
+
+### API Key 
+A valid API Key from Hashdd is required for the integration. To obtain an API Key, please see https://hashdd.com/#about and register for a free account.
+
+
+## Installation Instructions
+
+Installation instructions for integrations are provided on the [PolarityIO GitHub Page](https://polarityio.github.io/).
+
+## Polarity
+
+Polarity is a memory-augmentation platform that improves and accelerates analyst decision making.  For more information about the Polarity platform please see:
+
+https://polarity.io/
---
+++ integration.js
@@ -0,0 +1,172 @@
+'use strict';
+
+const request = require('request');
+const config = require('./config/config');
+const async = require('async');
+const fs = require('fs');
+
+let Logger;
+let requestWithDefaults;
+
+const MAX_PARALLEL_LOOKUPS = 10;
+
+function startup(logger) {
+  let defaults = {};
+  Logger = logger;
+
+  const { cert, key, passphrase, ca, proxy, rejectUnauthorized } = config.request;
+
+  if (typeof cert === 'string' && cert.length > 0) {
+    defaults.cert = fs.readFileSync(cert);
+  }
+
+  if (typeof key === 'string' && key.length > 0) {
+    defaults.key = fs.readFileSync(key);
+  }
+
+  if (typeof passphrase === 'string' && passphrase.length > 0) {
+    defaults.passphrase = passphrase;
+  }
+
+  if (typeof ca === 'string' && ca.length > 0) {
+    defaults.ca = fs.readFileSync(ca);
+  }
+
+  if (typeof proxy === 'string' && proxy.length > 0) {
+    defaults.proxy = proxy;
+  }
+
+  if (typeof rejectUnauthorized === 'boolean') {
+    defaults.rejectUnauthorized = rejectUnauthorized;
+  }
+
+  requestWithDefaults = request.defaults(defaults);
+}
+
+function doLookup(entities, options, cb) {
+  let lookupResults = [];
+  let tasks = [];
+
+  Logger.debug(entities);
+  entities.forEach((entity) => {
+    let postData = {"hash": entity.value, "api_key":  options.apiKey};
+    let requestOptions = {
+      method: 'POST',
+      uri: `https://api.hashdd.com/nsrl`,
+      body: postData,
+      json: true
+    };
+
+    Logger.trace({ requestOptions }, 'Request Options');
+
+    tasks.push(function(done) {
+      requestWithDefaults(requestOptions, function(error, res, body) {
+        Logger.trace({ body, status: res.statusCode });
+        let processedResult = handleRestError(error, entity, res, body);
+
+        if (processedResult.error) {
+          done(processedResult);
+          return;
+        }
+
+        done(null, processedResult);
+      });
+    });
+  });
+
+  async.parallelLimit(tasks, MAX_PARALLEL_LOOKUPS, (err, results) => {
+    if (err) {
+      Logger.error({ err: err }, 'Error');
+      cb(err);
+      return;
+    }
+
+    results.forEach((result) => {
+      if (result.body === null || result.body.length === 0 || result.body[result.entity.value].result === "NOT_FOUND") {
+        lookupResults.push({
+          entity: result.entity,
+          data: null
+        });
+      } else {
+        lookupResults.push({
+          entity: result.entity,
+          data: {
+            summary: [],
+            details: result.body[result.entity.value]
+          }
+        });
+      }
+    });
+
+    Logger.debug({ lookupResults }, 'Results');
+    cb(null, lookupResults);
+  });
+}
+
+function handleRestError(error, entity, res, body) {
+  let result;
+
+  if (error) {
+    return {
+      error: error,
+      detail: 'HTTP Request Error'
+    };
+  }
+
+  if (res.statusCode === 200 && body) {
+    // we got data!
+    result = {
+      entity: entity,
+      body: body
+    };
+  } else if (res.statusCode === 400) {
+    result = {
+      error: 'Bad Request',
+      detail: body.query_status
+    };
+  } else if (res.statusCode === 404) {
+    result = {
+      error: 'Not Found',
+      detail: body.query_status
+    };
+  } else if (res.statusCode === 429) {
+    result = {
+      error: 'Rate Limit Exceeded',
+      detail: body.query_status
+    };
+  } else {
+    result = {
+      error: 'Unexpected Error',
+      statusCode: res ? res.statusCode : 'Unknown',
+      detail: 'An unexpected error occurred'
+    };
+  }
+
+  return result;
+}
+
+function validateOption(errors, options, optionName, errMessage) {
+  if (
+    typeof options[optionName].value !== 'string' ||
+    (typeof options[optionName].value === 'string' && options[optionName].value.length === 0)
+  ) {
+    errors.push({
+      key: optionName,
+      message: errMessage
+    });
+  }
+}
+
+function validateOptions(options, callback) {
+  let errors = [];
+
+  validateOption(errors, options, 'apiKey', 'You must provide a valid API Key.');
+
+  callback(null, errors);
+}
+
+module.exports = {
+  doLookup: doLookup,
+  validateOptions: validateOptions,
+  startup: startup
+};
